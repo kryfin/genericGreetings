ecdfc2fa98391495944ff22cb01421355dc31285
kryfin <usman.m.razzaq@gmail.com>
2024-03-02 23:02:47 +0500

    new member

--- greetings.go
+++ greetings.go
@@ -5,6 +5,6 @@ import (
 )
 
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome back!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome NEw mEmber!", name)
 	return message
 }
